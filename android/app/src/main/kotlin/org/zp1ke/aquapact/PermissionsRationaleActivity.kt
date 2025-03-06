42753fd851889225f18abd24aa7d353ffdac88bc
Michel Atucha <zp1ke@outlook.com>
2025-03-05 19:38:54 -0500

    Update PermissionsRationaleActivity.kt

--- android/app/src/main/kotlin/org/zp1ke/aquapact/PermissionsRationaleActivity.kt
+++ android/app/src/main/kotlin/org/zp1ke/aquapact/PermissionsRationaleActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.zp1ke.aquapact.theme.AppTheme
@@ -96,6 +97,7 @@ fun PrivacyPolicyScreen() {
         Text(
             text = stringResource(R.string.privacy_policy_description),
             style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Justify,
         )
         Spacer(modifier = Modifier.height(32.dp))
         WebButton()
