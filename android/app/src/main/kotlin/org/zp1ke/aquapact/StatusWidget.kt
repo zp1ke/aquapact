d6edc028646397efee8df05f1bc57369f59cb22e
Matt Atcher <6463472+zp1ke@users.noreply.github.com>
2025-10-13 14:48:40 -0500

    Refactor Android status widget and remove unused imports
    - Removes unused `Intent` and `Build` imports in `StatusWidget.kt`.
    - Simplifies `PendingIntent` flag creation by removing the conditional SDK version check.
    - Removes `maxWidth` and `maxHeight` attributes from the widget configuration XML files.

--- android/app/src/main/kotlin/org/zp1ke/aquapact/StatusWidget.kt
+++ android/app/src/main/kotlin/org/zp1ke/aquapact/StatusWidget.kt
@@ -4,8 +4,6 @@ import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
-import android.content.Intent
-import android.os.Build
 import android.widget.RemoteViews
 import es.antonborri.home_widget.HomeWidgetPlugin
 
@@ -85,11 +83,7 @@ internal fun updateAppWidget(
             context,
             0,
             intent,
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-            } else {
-                PendingIntent.FLAG_UPDATE_CURRENT
-            }
+            PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
 
         // Set click listener on the entire widget
